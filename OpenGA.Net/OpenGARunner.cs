e72733f29dbdfb2164a1598877ba17a496d48fc3
Ahmed Arnaout <asarnaout@hotmail.com>
2025-08-17 13:55:26 -0400

    Calling the genetic repair function at the end

--- OpenGA.Net/OpenGARunner.cs
+++ OpenGA.Net/OpenGARunner.cs
@@ -185,6 +185,8 @@ public class OpenGARunner<T>
                 {
                     chromosome.Mutate();
                 }
+
+                chromosome.GeneticRepair();
             }
         }
     }
