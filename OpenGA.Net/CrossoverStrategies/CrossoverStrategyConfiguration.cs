0c6eaf6a732dde1bca3534d4ed13942ee7eda814
Ahmed Arnaout <asarnaout@hotmail.com>
2025-08-27 21:58:09 -0400

    Tentative: API design change for the crossover configuration

--- OpenGA.Net/CrossoverStrategies/CrossoverStrategyConfiguration.cs
+++ OpenGA.Net/CrossoverStrategies/CrossoverStrategyConfiguration.cs
@@ -7,29 +7,28 @@ public class CrossoverStrategyConfiguration<T>
     /// <summary>
     /// A point is chosen at random, and all the genes following that point are swapped between both parent chromosomes to produce two new child chromosomes
     /// </summary>
-    public BaseCrossoverStrategy<T> OnePointCrossover()
+    public CrossoverStrategyConfiguration<T> OnePointCrossover()
     {
         var result = new OnePointCrossoverStrategy<T>();
         CrossoverStrategies.Add(result);
-        return result;
+        return this;
     }
 
     /// <summary>
     /// A child chromosome is created by copying gene by gene from either parents (on a random basis).
     /// </summary>
-    public BaseCrossoverStrategy<T> UniformCrossover()
+    public CrossoverStrategyConfiguration<T> UniformCrossover()
     {
         var result = new UniformCrossoverStrategy<T>();
         CrossoverStrategies.Add(result);
-        return result;
+        return this;
     }
 
     /// <summary>
     /// Multiple points are chosen at random, and genes are alternated between parents at each crossover point to produce two new child chromosomes.
     /// </summary>
     /// <param name="numberOfPoints">The number of crossover points to use. Must be greater than 0.</param>
-    /// <returns>The configured k-point crossover strategy.</returns>
-    public BaseCrossoverStrategy<T> KPointCrossover(int numberOfPoints)
+    public CrossoverStrategyConfiguration<T> KPointCrossover(int numberOfPoints)
     {
         if (numberOfPoints <= 0)
         {
@@ -38,18 +37,18 @@ public class CrossoverStrategyConfiguration<T>
 
         var result = new KPointCrossoverStrategy<T>(numberOfPoints);
         CrossoverStrategies.Add(result);
-        return result;
+        return this;
     }
 
     /// <summary>
     /// Apply a custom strategy for crossing over chromosomes. Requires an instance of a subclass of <see cref="BaseCrossoverStrategy<T>">BaseCrossoverStrategy<T></see>
     /// to dictate which how a Couple of Chromosomes can reproduce a new set of Chromosomes.
     /// </summary>
-    public BaseCrossoverStrategy<T> CustomCrossover(BaseCrossoverStrategy<T> crossoverStrategy)
+    public CrossoverStrategyConfiguration<T> CustomCrossover(BaseCrossoverStrategy<T> crossoverStrategy)
     {
         ArgumentNullException.ThrowIfNull(crossoverStrategy, nameof(crossoverStrategy));
         CrossoverStrategies.Add(crossoverStrategy);
 
-        return crossoverStrategy;
+        return this;
     }
 }
